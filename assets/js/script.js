b0b6bdc7c575ce6dcee782ee8b394bb542a4c3ea
Giamo Lao <giamolao98@gmail.com>
2020-12-04 07:14:26 -0500

    comments

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,11 @@
+// To execute once the DOM is ready to be manipulated by JS
 $(document).ready(function()
 {
-
   // Check for click events on the navbar burger icon
   $(".navbar-burger").click(function()
   {
-
-      // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
-      $(".navbar-burger").toggleClass("is-active");
-      $(".navbar-menu").toggleClass("is-active");
-
+    // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+    $(".navbar-burger").toggleClass("is-active");
+    $(".navbar-menu").toggleClass("is-active");
   });
 });
